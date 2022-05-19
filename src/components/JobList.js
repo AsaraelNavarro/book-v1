d1ded314a31e0d2c668b46580603950a8ccdb2c5
Asarael Navarro <asarael.navarro@gmail.com>
2022-05-19 02:15:48 -0500

    add:  card changes

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -16,8 +16,8 @@ export default function JobList() {
                 <article key={jobs.id} className="card">
                     <div className="card__header">
                         <h3 className="card__header__title">{jobs.attributes.puesto} en {jobs.attributes.empresa} </h3>
-                        <small className="card__header__date">De: {jobs.attributes.fecha_entrada} -</small>
-                        <small className="card__header__date">A: {jobs.attributes.fecha_salida}</small>
+                        <small className="card__header__date">{jobs.attributes.fecha_entrada} - </small>
+                        <small className="card__header__date">{jobs.attributes.fecha_salida}</small>
                     </div>
                     <p className="card__content">{jobs.attributes.actividades}</p>
                     {/* <Link to={`/detalles/${jobs.id}`}>Ver Más</Link> */}
