2ab9a8ad0a1e1fd96f1311900657be101c27ac45
Asarael Navarro <asarael.navarro@gmail.com>
2022-05-21 18:49:02 -0500

    fix hash routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { /* BrowserRouter */ HashRouter } from "react-router-dom";
 
 //Page and layout imports
 /* import { Homepage } from './pages/Homepage';
